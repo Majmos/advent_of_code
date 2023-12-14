086a34cfd585abcaecf2ecc6275b3c191d7c367b
Majmos <mimsowy@gmail.com>
2023-12-14 22:30:29 +0100

    part 2 day 5 aoc 2023 brute force

--- aoc_2023/python/day_5/day_5.py
+++ aoc_2023/python/day_5/day_5.py
@@ -4,6 +4,7 @@ import re
 def find_map(input: str):
     return map_source_to_destination(input.split(':\n')[1].strip().split('\n'))
 
+
 def map_source_to_destination(list_to_map: list[str]):
     mapped_list = []
     for line in list_to_map:
@@ -11,6 +12,7 @@ def map_source_to_destination(list_to_map: list[str]):
         mapped_list.append((int(destination), int(source), int(length)))
 
     return mapped_list
+
 
 def find_seed_location(seed: int, 
                        seed_to_soil_map: list[tuple[str, str, str]],
@@ -56,7 +58,7 @@ def find_seed_location(seed: int,
             location = destination - source + humidity
 
     return location
-    
+
 
 def part_1(input: str):
     groups = input.split('\n\n')
@@ -92,6 +94,43 @@ def part_1(input: str):
     
     return answer
 
+
+def part_2_brute_force(input: str):
+    groups = input.split('\n\n')
+
+    seeds = re.search('(?<=seeds: ).*', groups[0]).group().split()
+
+    seed_to_soil_map = find_map(groups[1])
+
+    soil_to_fertilizer_map = find_map(groups[2])
+
+    fertilizer_to_water_map = find_map(groups[3])
+
+    water_to_light_map = find_map(groups[4])
+
+    light_to_temperature_map = find_map(groups[5])
+
+    temperature_to_humidity_map = find_map(groups[6])
+
+    humidity_to_location_map = find_map(groups[7])
+
+    answer = sys.maxsize
+    for i in range(0, len(seeds), 2):
+        for j in range(int(seeds[i + 1])):
+            temp = find_seed_location(int(seeds[i]) + j,
+                                      seed_to_soil_map,
+                                      soil_to_fertilizer_map,
+                                      fertilizer_to_water_map,
+                                      water_to_light_map,
+                                      light_to_temperature_map,
+                                      temperature_to_humidity_map,
+                                      humidity_to_location_map)
+            if int(temp) < answer:
+                answer = int(temp)
+    
+    return answer
+
+
 def main():
     if len(sys.argv) == 2:
         input_file = str(sys.argv[1])
@@ -101,7 +140,7 @@ def main():
     with open(f'{input_file}', 'r') as f:
         input = f.read()
 
-    print(part_1(input))
+    print(part_2(input))
 
 if __name__ == "__main__":
     main()
