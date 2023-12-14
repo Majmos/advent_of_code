f30fda5c5d17f1d088c8b5ff3cc035a780b07838
Majmos <mimsowy@gmail.com>
2023-12-15 00:11:10 +0100

    completed part 2 of day 5 aoc 2023

--- aoc_2023/python/day_5/day_5.py
+++ aoc_2023/python/day_5/day_5.py
@@ -5,6 +5,10 @@ def find_map(input: str):
     return map_source_to_destination(input.split(':\n')[1].strip().split('\n'))
 
 
+def find_map_reverse(input: str):
+    return map_destination_to_source(input.split(':\n')[1].strip().split('\n'))
+
+
 def map_source_to_destination(list_to_map: list[str]):
     mapped_list = []
     for line in list_to_map:
@@ -14,50 +18,25 @@ def map_source_to_destination(list_to_map: list[str]):
     return mapped_list
 
 
-def find_seed_location(seed: int, 
-                       seed_to_soil_map: list[tuple[str, str, str]],
-                       soil_to_fertilizer_map: list[tuple[str, str, str]],
-                       fertilizer_to_water_map: list[tuple[str, str, str]],
-                       water_to_light_map: list[tuple[str, str, str]],
-                       light_to_temperature_map: list[tuple[str, str, str]],
-                       temperature_to_humidity_map: list[tuple[str, str, str]],
-                       humidity_to_location_map: list[tuple[str, str, str]]):
-    soil = seed
-    for destination, source, length in seed_to_soil_map:
-        if seed in range(source, source + length):
-            soil = destination - source + seed
+def map_destination_to_source(list_to_map: list[str]):
+    mapped_list = []
+    for line in list_to_map:
+        (destination, source, length) = line.split()
+        mapped_list.append((int(source), int(destination), int(length)))
+
+    return mapped_list
+
+
+def map_given_value(value: int, maps: list[list[tuple[str, str, str]]]):
+    mapped_value: int = value
+    for map in maps:
+        temp: int = mapped_value
+        for destination, source, length in map:
+            if mapped_value in range(int(source), int(source) + int(length)):
+                temp = int(destination) - int(source) + mapped_value
+        mapped_value = temp
     
-    fertilizer = soil
-    for destination, source, length in soil_to_fertilizer_map:
-        if soil in range(source, source + length):
-            fertilizer = destination - source + soil
-
-    water = fertilizer
-    for destination, source, length in fertilizer_to_water_map:
-        if fertilizer in range(source, source + length):
-            water = destination - source + fertilizer
-
-    light = water
-    for destination, source, length in water_to_light_map:
-        if water in range(source, source + length):
-            light = destination - source + water
-
-    temperature = light
-    for destination, source, length in light_to_temperature_map:
-        if light in range(source, source + length):
-            temperature = destination - source + light
-
-    humidity = temperature
-    for destination, source, length in temperature_to_humidity_map:
-        if temperature in range(source, source + length):
-            humidity = destination - source + temperature    
-
-    location = humidity
-    for destination, source, length in humidity_to_location_map:
-        if humidity in range(source, source + length):
-            location = destination - source + humidity
-
-    return location
+    return mapped_value
 
 
 def part_1(input: str):
@@ -65,30 +44,13 @@ def part_1(input: str):
 
     seeds = re.search('(?<=seeds: ).*', groups[0]).group().split()
 
-    seed_to_soil_map = find_map(groups[1])
-
-    soil_to_fertilizer_map = find_map(groups[2])
-
-    fertilizer_to_water_map = find_map(groups[3])
-
-    water_to_light_map = find_map(groups[4])
-
-    light_to_temperature_map = find_map(groups[5])
-
-    temperature_to_humidity_map = find_map(groups[6])
-
-    humidity_to_location_map = find_map(groups[7])
+    maps = []
+    for i in range(1, 8):
+        maps.append(find_map(groups[i]))
 
     answer = sys.maxsize
     for seed in seeds:
-        temp = find_seed_location(int(seed),
-                           seed_to_soil_map,
-                           soil_to_fertilizer_map,
-                           fertilizer_to_water_map,
-                           water_to_light_map,
-                           light_to_temperature_map,
-                           temperature_to_humidity_map,
-                           humidity_to_location_map)
+        temp = map_given_value(int(seed), maps) 
         if int(temp) < answer:
             answer = int(temp)
     
@@ -100,38 +62,42 @@ def part_2_brute_force(input: str):
 
     seeds = re.search('(?<=seeds: ).*', groups[0]).group().split()
 
-    seed_to_soil_map = find_map(groups[1])
-
-    soil_to_fertilizer_map = find_map(groups[2])
-
-    fertilizer_to_water_map = find_map(groups[3])
-
-    water_to_light_map = find_map(groups[4])
-
-    light_to_temperature_map = find_map(groups[5])
-
-    temperature_to_humidity_map = find_map(groups[6])
-
-    humidity_to_location_map = find_map(groups[7])
+    maps = []
+    for i in range(1, 8):
+        maps.append(find_map(groups[i]))
 
     answer = sys.maxsize
     for i in range(0, len(seeds), 2):
         for j in range(int(seeds[i + 1])):
-            temp = find_seed_location(int(seeds[i]) + j,
-                                      seed_to_soil_map,
-                                      soil_to_fertilizer_map,
-                                      fertilizer_to_water_map,
-                                      water_to_light_map,
-                                      light_to_temperature_map,
-                                      temperature_to_humidity_map,
-                                      humidity_to_location_map)
+            temp = map_given_value(int(seeds[i]) + j, maps)
             if int(temp) < answer:
                 answer = int(temp)
     
     return answer
 
 
+def part_2(input: str):
+    groups = input.split('\n\n')
+
+    seeds = re.search('(?<=seeds: ).*', groups[0]).group().split()
+
+    maps: list[tuple[str, str, str]] = []
+    for i in range(7, 0, -1):
+        maps.append(find_map_reverse(groups[i]))
+
+    location_to_humidity_map = sorted(maps[0], key = lambda a: a[1], reverse=True)
+
+    for j in range(location_to_humidity_map[0][1]):
+        seed = map_given_value(j, maps)
+        for i in range(0, len(seeds), 2):
+            if seed in range(int(seeds[i]), int(seeds[i]) + int(seeds[i + 1])):
+                return j
+    
+    return 0
+
+
 def main():
+
     if len(sys.argv) == 2:
         input_file = str(sys.argv[1])
     else:
